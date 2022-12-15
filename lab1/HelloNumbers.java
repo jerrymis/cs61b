10178d117474ff24a3e7db47fbc5017bb14bfe0f
jerrymis <1831433004@qq.com>
2022-12-15 13:27:28 +0800

    lab1 finished

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+	int temp = 0;
+        while (x < 10) {
+            System.out.print(temp + " ");
+            x = x + 1;
+	    temp = temp + x;
+        }
+    }
+}
